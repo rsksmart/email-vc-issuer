e3e50fe12a2137839f33fbe7cf7899e08339452e
Ilan Olkies <ilanolkies@outlook.com>
2020-12-05 20:14:44 -0300

    Fix VerificationCodeChecker interface

--- model/VerificationCodeChecker.test.ts
+++ model/VerificationCodeChecker.test.ts
@@ -1,40 +1,44 @@
-import VerificationCodeChecker from './VerificationCodeChecker'
+import VerificationCodeChecker, { CODE_NOT_GENERATED_ERROR_MESSAGE } from './VerificationCodeChecker'
 
 describe('VerificationCodeChecker', () => {
   test('checks the code is correct', () => {
     const checker = new VerificationCodeChecker()
 
     const did = 'did:ethr:rsk:0x87eb390df1e05ef0560e387206f5997034cd6f28'
-    const code = checker.getCodeFor(did)
+    const code = checker.generateCodeFor(did)
 
-    expect(checker.checkCodeFor(did, 'INVALID CODE')).toBeFalsy()
-    expect(checker.checkCodeFor(did, code)).toBeTruthy()
+    expect(checker.getCodeOf(did)).toEqual(code)
   })
 
   test('resets code for the same did', () => {
-
     const checker = new VerificationCodeChecker()
 
     const did = 'did:ethr:rsk:0x87eb390df1e05ef0560e387206f5997034cd6f28'
-    const code = checker.getCodeFor(did)
-    const secondCode = checker.getCodeFor(did)
+    const code = checker.generateCodeFor(did)
+    const secondCode = checker.generateCodeFor(did)
 
     expect(code).not.toEqual(secondCode)
-    expect(checker.checkCodeFor(did, code)).toBeFalsy()
-    expect(checker.checkCodeFor(did, secondCode)).toBeTruthy()
+    expect(checker.getCodeOf(did)).toEqual(secondCode)
   })
 
   test('sends different codes for the different dids', () => {
-
     const checker = new VerificationCodeChecker()
 
     const did = 'did:ethr:rsk:0x87eb390df1e05ef0560e387206f5997034cd6f28'
     const did2 = 'did:ethr:rsk:0xa31a90984e7aeb66929759d192793736600687bc'
-    const code = checker.getCodeFor(did)
-    const code2 = checker.getCodeFor(did2)
+    const code = checker.generateCodeFor(did)
+    const code2 = checker.generateCodeFor(did2)
 
     expect(code).not.toEqual(code2)
-    expect(checker.checkCodeFor(did, code2)).toBeFalsy()
-    expect(checker.checkCodeFor(did2, code)).toBeFalsy()
+    expect(checker.getCodeOf(did)).toEqual(code)
+    expect(checker.getCodeOf(did2)).toEqual(code2)
+  })
+
+  test('does not answer code when not generated', () => {
+    const checker = new VerificationCodeChecker()
+
+    const did = 'did:ethr:rsk:0x87eb390df1e05ef0560e387206f5997034cd6f28'
+
+    expect(() => checker.getCodeOf(did)).toThrowError(CODE_NOT_GENERATED_ERROR_MESSAGE)
   })
 })
--- model/VerificationCodeChecker.ts
+++ model/VerificationCodeChecker.ts
@@ -1,5 +1,7 @@
 import { randomBytes }  from 'crypto'
 
+export const CODE_NOT_GENERATED_ERROR_MESSAGE = 'Generate code first'
+
 export default class VerificationCodeChecker {
   codes: Map<string, string>
 
@@ -7,13 +9,15 @@ export default class VerificationCodeChecker {
     this.codes = new Map()
   }
 
-  getCodeFor(did: string) {
+  generateCodeFor(did: string) {
     const code = randomBytes(32).toString('hex')
     this.codes.set(did, code)
     return code
   }
 
-  checkCodeFor(did: string, code: string) {
-    return code === this.codes.get(did)
+  getCodeOf(did: string) {
+    const code = this.codes.get(did)
+    if (!code) throw new Error(CODE_NOT_GENERATED_ERROR_MESSAGE)
+    return code
   }
 }
